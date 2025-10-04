160e76c53b9a81e36f84907d1f3eab212eeb5b23
Peter Metz <hallo@petermetz.de>
2025-10-04 18:21:46 +0200

    Added checks for duplicates

--- csv-validate.py
+++ csv-validate.py
@@ -18,11 +18,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
 
+    SeenIBANs = { }
+    SeenMandatsreferenz = { }
     with open(vars(args)['input-file.csv']) as csvfile:
         reader = csv.DictReader(csvfile, delimiter=',')
         for row in reader:
-            if row['IBAN'] != '':
+            if row['IBAN'] != '' and row['Mandatsreferenz'][:4] == 'REF-':
                 try:
+                    if row['IBAN'] not in SeenIBANs:
+                        SeenIBANs[row['IBAN']] = { 'Name': row['Name des Zahlungspflichtigen'], 'Count': 1 }
+                    else:
+                        SeenIBANs[row['IBAN']]['Count'] += 1
+                    if row['Mandatsreferenz'] not in SeenMandatsreferenz:
+                        SeenMandatsreferenz[row['Mandatsreferenz']] = { 'Name': row['Name des Zahlungspflichtigen'], 'Count': 1 }
+                    else:
+                        SeenMandatsreferenz[row['Mandatsreferenz']]['Count'] += 1
+
                     iban = IBAN(row['IBAN'])
                     if row['SWIFT/BIC'] == '':
                         print(f"  - for {row['Name des Zahlungspflichtigen']}, given {iban}, BIC should be '{iban.bic}'")
@@ -32,4 +43,12 @@ if __name__ == "__main__":
 
                 except Exception as e:
                     print(f"  *** Caught exception {type(e)} / {e} while validating {row['Name des Zahlungspflichtigen']}")
-            elif args.verbosity > 1: print(f"  - entry for {row['Name des Zahlungspflichtigen']} skipped due to invalid Matdatsreferenz ({row['Mandatsreferenz']})")
+            else:
+                print(f"  - entry for {row['Name des Zahlungspflichtigen']} skipped : Matdatsreferenz = '{row['Mandatsreferenz']}', IBAN = '{row['IBAN']}'")
+
+    for key, val in SeenIBANs.items():
+        if val['Count'] > 1:
+            print(f"  - duplicate IBAN {key} found: {val}")
+    for key, val in SeenMandatsreferenz.items():
+        if val['Count'] > 1:
+            print(f"  - duplicate Mandatsreferenz {key} found: {val}")            
